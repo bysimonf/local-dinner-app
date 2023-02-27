f700674bdaac69a84e7a1abb260ef1973b70395d
Simon Frohnauer <simon.frohnauer@gmx.de>
2023-02-27 16:12:08 +0100

    added dinner model logic

--- app/controllers/dinners_controller.rb
+++ app/controllers/dinners_controller.rb
@@ -8,8 +8,21 @@ class DinnersController < ApplicationController
   end
 
   def create
+    @dinner = Dinner.new(dinner_params)
+    if @dinner.save
+      redirect_to dinner_path(@dinner)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @dinner = Dinner.find(params[:id])
+  end
+
+  private
+
+  def dinner_params
+    params.require(:dinner).permit(:title, :category, :date)
   end
 end
