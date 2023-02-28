88942c4de172e6213f805a886c5549494c81b0c5
bethelhem abegaz <bethelhemmesfin121@gmail.com>
2023-02-28 17:00:23 +0100

    commit schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_105342) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_115418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_105342) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.integer "capacity"
+    t.string "image_path"
     t.index ["user_id"], name: "index_dinners_on_user_id"
   end
 
